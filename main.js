9a55f58d747c83f88af9ce60a3327ca9f82d3f66
João Lucas <135267397+Jslus12@users.noreply.github.com>
2026-06-10 20:31:10 -0300

    JS commit 1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// Arquivo para código javascript
+const API = 'https://6a1f56a9b79eec0d6cf0a932.mockapi.io/api/v1/users';
+const tbody = document.getElementById('tbody');
+
+async function carregarMateriais() {
+  const res = await fetch(API);
+  const data = await res.json();
+  tbody.innerHTML = data.map(i => `
+    <tr><td>${i.name}</td><td>${i.quantidade}</td></tr>
+  `).join('');
+}
