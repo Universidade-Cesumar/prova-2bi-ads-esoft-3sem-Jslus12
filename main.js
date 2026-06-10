7ebc3489fcf2263ad7e80e812d3506f1616369bf
João Lucas <135267397+Jslus12@users.noreply.github.com>
2026-06-10 20:31:36 -0300

    Commit js 2

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ async function carregarMateriais() {
     <tr><td>${i.name}</td><td>${i.quantidade}</td></tr>
   `).join('');
 }
+
+
+document.getElementById('btn-cadastrar').addEventListener('click', async () => {
+  const nome = document.getElementById('input-nome').value.trim();
+  const quantidade = document.getElementById('input-quantidade').value.trim();
+  if (!nome || !quantidade) return alert('Preencha os campos');
+
+  await fetch(API, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: nome, quantidade: Number(quantidade) })
+  });
+
+  document.getElementById('input-nome').value = '';
+  document.getElementById('input-quantidade').value = '';
+  carregarMateriais();
+});
+
+carregarMateriais();
