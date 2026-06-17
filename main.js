1847e12e28c2bd41b7ad11a41a876140dfae8757
João Lucas <135267397+Jslus12@users.noreply.github.com>
2026-06-17 20:07:59 -0300

    Adicionando funções

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ function validarRetirada(estoqueAtual, quantidadeRetirada) {
 }
 
 async function carregarMateriais() {
+   const res = await fetch(API);
+  const data = await res.json();
+  renderizarTabela(data);
+}
+
+function renderizarTabela(data) {
+tbody.innerHTML = data.map(i => `
+    <tr>
+      <td>${i.name}</td>
+      <td>${i.quantidade}</td>
+      <td>
+        <div class="retirada-group">
+          <input type="number" id="input-retirada" min="1" placeholder="0"
+            data-id="${i.id}" data-estoque="${i.quantidade}" />
+          <button class="btn-baixar" data-id="${i.id}">Baixar</button>
+        </div>
+      </td>
+      <td><button class="btn-excluir" data-id="${i.id}">Excluir</button></td>
+    </tr>
+`).join('');
+
